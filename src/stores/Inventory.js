51a162e899620e7dafd159c5ec5bc1af71b4d51e
madfozjer <fozerion00@proton.me>
2025-07-01 14:50:28 +0200

    robust fix for now

--- src/stores/Inventory.js
+++ src/stores/Inventory.js
@@ -21,19 +21,27 @@ export const useInventoryStore = defineStore('inventory', {
       // this.clearInventorySave()
       if (
         localStorage.getItem('hunters') != null &&
-        localStorage.getItem('activeHunters') != null &&
-        JSON.parse(localStorage.getItem('hunters')).length <= 0 &&
-        JSON.parse(localStorage.getItem('activeHunters')).length <= 0
+        localStorage.getItem('activeHunters') != null
       ) {
+        if (
+          JSON.parse(localStorage.getItem('hunters')).length <= 0 &&
+          JSON.parse(localStorage.getItem('activeHunters')).length <= 0
+        ) {
+          this.addHunter('Jacob')
+          this.hunters[0].id = 1
+          this.addWeapon('Wooden Sword')
+          console.log('Initialized inventory')
+        } else {
+          this.currentHunterID = parseInt(localStorage.getItem('currentHunterID'))
+          this.currentWeaponID = parseInt(localStorage.getItem('currentWeaponID'))
+          this.hunters = JSON.parse(localStorage.getItem('hunters'))
+          this.weapons = JSON.parse(localStorage.getItem('weapons'))
+        }
+      } else {
         this.addHunter('Jacob')
         this.hunters[0].id = 1
         this.addWeapon('Wooden Sword')
         console.log('Initialized inventory')
-      } else {
-        this.currentHunterID = parseInt(localStorage.getItem('currentHunterID'))
-        this.currentWeaponID = parseInt(localStorage.getItem('currentWeaponID'))
-        this.hunters = JSON.parse(localStorage.getItem('hunters'))
-        this.weapons = JSON.parse(localStorage.getItem('weapons'))
       }
     },
 
